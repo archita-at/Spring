640cb45476b7c81075d8868fabba7c7b17509ba3
Archita Totla <68774069+archita-at@users.noreply.github.com>
2021-01-01 20:58:25 +0530

    Basic Spring Project

---
+++ demo/demo/src/main/java/com/example/demo/ui/controller/UserController.java
@@ -0,0 +1,80 @@
+package com.example.demo.ui.controller;
+
+import com.example.demo.ui.model.request.UserDetailsRequestModel;
+import com.example.demo.ui.model.response.UserRest;
+
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("users") // http://localhost:8080/users
+public class UserController {
+
+    @InitBinder
+	public void initBinder(WebDataBinder databinder) {
+		StringTrimmerEditor ste = new StringTrimmerEditor(true);
+		
+		databinder.registerCustomEditor(String.class, ste);
+	}
+    
+    @GetMapping
+    public String getUsers(@RequestParam(value="page", defaultValue="1") int page, 
+                            @RequestParam(value="limit", defaultValue="50") int limit,
+                            @RequestParam(value="sort", defaultValue="desc",required=false) String sort){
+        return "getUsers was called with page = " + page + " ,limit = " + limit + " and sort = " + sort;
+    }
+
+    @GetMapping(path="/{userId}", 
+                produces={
+                    MediaType.APPLICATION_XML_VALUE, 
+                    MediaType.APPLICATION_JSON_VALUE
+                })
+    public ResponseEntity<UserRest> getUser(@PathVariable String userId){
+        UserRest returnValue = new UserRest();
+        returnValue.setEmail("test@xyz.com");
+        returnValue.setFirstName("Archita");
+        returnValue.setLastName("Totla");
+        return new ResponseEntity<UserRest>(HttpStatus.BAD_REQUEST);
+    }
+    
+    @PostMapping(consumes ={
+        MediaType.APPLICATION_XML_VALUE, 
+        MediaType.APPLICATION_JSON_VALUE
+        },
+        produces={
+            MediaType.APPLICATION_XML_VALUE, 
+            MediaType.APPLICATION_JSON_VALUE
+        })
+    public ResponseEntity<UserRest> createUser(@Valid @RequestBody UserDetailsRequestModel userDetails){
+        UserRest returnValue = new UserRest();
+        returnValue.setEmail(userDetails.getEmail());
+        returnValue.setFirstName(userDetails.getFirstName());
+        returnValue.setLastName(userDetails.getLastName());
+        return new ResponseEntity<UserRest>(returnValue, HttpStatus.OK);
+    }
+    
+    @PutMapping
+    public String updateUser(){
+        return "updateUser was called";
+    }
+
+    @DeleteMapping
+    public String deleteUser(){
+        return "deleteUser was called";
+    }
+}
